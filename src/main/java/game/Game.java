2a150fa4753f5ba0a3be74192224bdc0db8f4439
DimitarSivrev <96288224+DimitarSivrev@users.noreply.github.com>
2022-10-30 18:06:25 +0000

    Add files via upload

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,12 @@
+package game;
+
+public class Game {
+    private int numberOfPlayers;
+    private Card[] pack;
+    private Player[] players;
+    private CardDeck[] decks;
+    
+    public String winner(){
+        return "winner";
+    }
+}
