4bb3e55657273268df748bb3508a38b38e6d5ff9
DimitarSivrev <96288224+DimitarSivrev@users.noreply.github.com>
2022-11-05 23:45:41 +0000

    Update Game.java

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,6 +1,7 @@
 package game;
 
-import java.io.File;
+import java.util.*;
+import java.io.*;
 
 public class Game {
     private int numberOfPlayers;
@@ -9,20 +10,79 @@ public class Game {
     private CardDeck[] decks;
     private File inputFile;
 
-    public Game(int numberOfPlayers, Card[] pack, Player[] players, CardDeck[] decks, File inputFile) {
-        this.numberOfPlayers = numberOfPlayers;
-        this.pack = pack;
-        this.players = players;
-        this.decks = decks;
-        this.inputFile = inputFile;
+    Game(){
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Please enter the number of players: ");
+            try {
+                int number = scanner.nextInt();
+                if (number > 1) {
+                    numberOfPlayers = number;
+                    break;
+                } else {
+                    System.out.println("Number of players must be more than 1.");
+                }
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("Number of players must be an integer.");
+            }
+        }
+
+        pack = new Card[8*numberOfPlayers];
+        while (true) {
+            System.out.println("Please enter location of pack to load:");
+            String fileName = scanner.next();
+            File inputFile = new File(fileName);
+            if(inputFile.exists()){
+                if (readFile(fileName) == true){
+                    break;
+                }
+                
+            }else if(pack.length != 8 * numberOfPlayers){
+                System.out.println(String.format("The file doesn't have the right number of cards."));
+            }else {
+                System.out.println(String.format("The file doesn't exist."));
+            }
+        }
+
+        scanner.close();
+        
     }
-
-    public Game(int numberOfPlayers){
-        this.numberOfPlayers = numberOfPlayers;
-    }
-
-    public Game(){
-
+    private boolean readFile(String fileName){
+        try {
+            int index = 0;
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (index < (8 * numberOfPlayers)) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                } else {
+                    int value = 0;
+                    try {
+                        value = Integer.parseInt(line);
+                        if (value > 0) {
+                            pack[index] = new Card(value);
+                        } else {
+                            System.out.println("On line " + (index + 1) + " there was a number less that 1");
+                            reader.close();
+                            return false;
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("On line " + (index +1 ) + " there was a wrong format");
+                        reader.close();
+                        return false;
+                    }
+                    
+                }
+                index++;
+                
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            return false;
+        }
+        return true;
     }
 
     public void createPlayersAndDecks(){
